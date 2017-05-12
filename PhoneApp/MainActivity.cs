7516cd15f91562ae73494bab75fff4c3d6081d84
Israel Calderón <icalderond@Israels-MacBook-Pro.local>
2017-05-12 09:14:56 -0500

    App probada y ejecutada correctamente

--- PhoneApp/MainActivity.cs
+++ PhoneApp/MainActivity.cs
@@ -7,7 +7,6 @@ namespace PhoneApp
 	[Activity(Label = "PhoneApp", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity
 	{
-		int count = 1;
 
 		protected override void OnCreate(Bundle savedInstanceState)
 		{
@@ -15,13 +14,50 @@ namespace PhoneApp
 
 			// Set our view from the "main" layout resource
 			SetContentView(Resource.Layout.Main);
+			var PhoneNumberText = FindViewById<EditText>(Resource.Id.PhoneNumberText);
+			var TranslateButton = FindViewById<Button>(Resource.Id.TranslateButton);
+			var CallButton = FindViewById<Button>(Resource.Id.CallButton);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+			CallButton.Enabled = false;
 
-			button.Click += delegate { button.Text = $"{count++} clicks!"; };
+			var TranslatedNumber = string.Empty;
+
+			TranslateButton.Click += (object sender, System.EventArgs e) =>
+		   {
+			   var Translator = new PhoneTranslator();
+			   TranslatedNumber = Translator.ToNumber(PhoneNumberText.Text);
+			   if (string.IsNullOrWhiteSpace(TranslatedNumber))
+			   {
+				   // No hay número a llamar
+				   CallButton.Text = "Llamar";
+				   CallButton.Enabled = false;
+			   }
+			   else
+			   {
+				   CallButton.Enabled = true;
+			   }
+		   };
+			CallButton.Click += (object sender, System.EventArgs e) =>
+			 {
+				 // Intentar marcar el número telefónico
+				 var CallDialog = new AlertDialog.Builder(this);
+				 CallDialog.SetMessage($"Llamar al número {TranslatedNumber}?");
+				 CallDialog.SetNeutralButton("Llamar", delegate
+				 {
+					 // Crear un intento para marcar el número telefónico
+					 var CallIntent =
+						new Android.Content.Intent(Android.Content.Intent.ActionCall);
+					 CallIntent.SetData(
+											  Android.Net.Uri.Parse($"tel:{TranslatedNumber}"));
+
+					 StartActivity(CallIntent);
+				 });
+				 CallDialog.SetNegativeButton("Cancelar", delegate { });
+				 // Mostrar el cuadro de diálogo al usuario y esperar una respuesta.
+				 CallDialog.Show();
+			 };
 		}
+
 	}
 }
 
---
+++ PhoneApp/PhoneTranslator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace PhoneApp
+{
+	public class PhoneTranslator
+	{
+		string Letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		string Numbers = "22233344455566677778889999";
+		public PhoneTranslator()
+		{
+		}
+		public string ToNumber(string alfanumericPhoneNumber)
+		{
+			var NumericPhoneNumber = new StringBuilder();
+			if (!string.IsNullOrWhiteSpace(alfanumericPhoneNumber))
+			{
+				alfanumericPhoneNumber = alfanumericPhoneNumber.ToUpper();
+				foreach (var c in alfanumericPhoneNumber)
+				{
+					if ("0123456789".Contains(c.ToString()))
+					{
+						NumericPhoneNumber.Append(c);
+					}
+					else
+					{
+						var Index = Letters.IndexOf(c);
+						if (Index >= 0)
+							NumericPhoneNumber.Append(Numbers[Index]);
+					}
+				}
+			}
+
+			return NumericPhoneNumber.ToString();
+		}
+	}
+}
